6fc2e393e3463767da05ec3a8046646b886afd46
neudabei <rschultzejena@gmail.com>
2015-08-28 22:52:20 +0100

    fixed feature test

--- spec/features/user_interacts_with_newsletter.rb
+++ spec/features/user_interacts_with_newsletter.rb
@@ -1,6 +1,11 @@
 require 'spec_helper'
 
 feature "User interacts with newsletter" do
+  
+  background do
+    Category.create(name: "Bitcoin")
+  end
+
   scenario "user creates new newsletter" do
     # sign in
     login
@@ -11,9 +16,7 @@ feature "User interacts with newsletter" do
     fill_in "newsletter[description]", with: "This is what the newsletter is all about..."
     fill_in "newsletter[website_url]", with: "www.domainofnewsletter.com"
     fill_in "newsletter[signup_url]", with: "www.domainofnewsletter.com/signup"
-    #check('newsletter_category_ids_6')
-    #page.check("value='6'")
-    find(:css, "for='newsletter_category_ids_7'").set(true)
+    check('Bitcoin')
     click_button "Submit"
     # see newsletter is in index queue
   end
