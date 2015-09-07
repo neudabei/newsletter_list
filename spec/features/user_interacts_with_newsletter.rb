4e70516a3d339d5aaac1e4f9721d140ba81b401b
neudabei <rschultzejena@gmail.com>
2015-09-07 15:43:08 +0100

    add feature test for voting functionality

--- spec/features/user_interacts_with_newsletter.rb
+++ spec/features/user_interacts_with_newsletter.rb
@@ -6,22 +6,33 @@ feature "User interacts with newsletter" do
     Category.create(name: "Bitcoin")
   end
 
-  scenario "user creates new newsletter" do
+  scenario "user creates new newsletter and votes on it" do
+    newsletter = Fabricate(:newsletter)
     # sign in
     login
     # go to add newsletter
     click_link "Add newsletter"
     # fill out form and submit
-    fill_in "newsletter[name]", with: "The great newsletter"
-    fill_in "newsletter[description]", with: "This is what the newsletter is all about..."
-    fill_in "newsletter[website_url]", with: "www.domainofnewsletter.com"
-    fill_in "newsletter[signup_url]", with: "www.domainofnewsletter.com/signup"
-    check('Bitcoin')
+    fill_in "newsletter[name]", with: "#{newsletter.name}"
+    fill_in "newsletter[description]", with: "#{newsletter.description}"
+    fill_in "newsletter[website_url]", with: "#{newsletter.website_url}"
+    fill_in "newsletter[signup_url]", with: "#{newsletter.signup_url}"
+    check("Bitcoin")
     click_button "Submit"
     # see newsletter is in index queue
+    click_link "View newsletters"
+    expect(page).to have_content("#{newsletter.name}")
+    # vote on newsletter
+    click_link "vote_#{newsletter.id}"
+    expect(page).to have_content("1")
+
   end
 
   scenario "user votes on newsletter" do
+
+    # login
+    # puts page.body
+    # click_link "vote_1"
 
   end
 
