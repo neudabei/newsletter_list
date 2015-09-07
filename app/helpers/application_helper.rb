1039aa452164eab5e130fd4e1c316ea8d3191374
neudabei <rschultzejena@gmail.com>
2015-09-07 14:36:40 +0100

    fix url adjustment helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,9 @@
 module ApplicationHelper
+<<<<<<< Updated upstream
 
+=======
+  def fix_url(str)
+    str.starts_with?('http://') || str.starts_with?('https://') ? str : "http://#{str}"
+  end
+>>>>>>> Stashed changes
 end
