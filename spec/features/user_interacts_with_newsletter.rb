246d7a5f42fbb474ee5bdd9a99f1959f01db41ba
neudabei <rschultzejena@gmail.com>
2015-09-08 11:08:37 +0100

    extract feature tests into methods for better readability

--- spec/features/user_interacts_with_newsletter.rb
+++ spec/features/user_interacts_with_newsletter.rb
@@ -10,22 +10,27 @@ feature "User interacts with newsletter" do
     newsletter = Fabricate(:newsletter)
     # sign in
     login
-    # go to add newsletter
+    add_newsletter
+    vote_on_newsletter
+  end
+
+  def add_newsletter
     click_link "Add newsletter"
-    # fill out form and submit
     fill_in "newsletter[name]", with: "#{newsletter.name}"
     fill_in "newsletter[description]", with: "#{newsletter.description}"
     fill_in "newsletter[website_url]", with: "#{newsletter.website_url}"
     fill_in "newsletter[signup_url]", with: "#{newsletter.signup_url}"
     check("Bitcoin")
     click_button "Submit"
+
     # see newsletter is in index queue
     click_link "View newsletters"
     expect(page).to have_content("#{newsletter.name}")
-    # vote on newsletter
-    click_link "vote_#{newsletter.id}"
-    expect(page).to have_content("1")
-
   end
 
+  def vote_on_newsletter
+    click_link "vote_#{newsletter.id}"
+    expect(page).to have_content("1")
+  end
+    
 end
